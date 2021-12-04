648544852b264f3289efd298fcd710c952f624b0
MercyFlesh <fleshmercy@gmail.com>
2021-12-04 03:04:38 +0300

    refactoring DownloadBtn_Click

---
+++ VulnParser/Models/DownloadService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Windows;
+
+
+namespace VulnParser.Models
+{
+    public class DownloadService
+    {
+        public static void Download(string downloadAddr, string filePath)
+        {
+            try
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    webClient.DownloadFileAsync(new Uri(downloadAddr), filePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error: " + ex.Message);
+            }
+        }
+    }
+}
--- VulnParser/Views/DownloadWIndow.xaml.cs
+++ VulnParser/Views/DownloadWIndow.xaml.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Net;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using VulnParser.ViwModels;
+using VulnParser.Models;
 
 namespace VulnParser.Views
 {
-    /// <summary>
-    /// Логика взаимодействия для DownloadWIndow.xaml
-    /// </summary>
     public partial class DownloadWIndow : Window
     {
         public DownloadWIndow()
@@ -29,22 +14,8 @@ namespace VulnParser.Views
         private void downloadBtn_Click(object sender, RoutedEventArgs e)
         {
             downloadBtn.IsEnabled = false;
-            
-            try
-            {
-                using(WebClient webClient = new WebClient())
-                {
-                    webClient.DownloadFileAsync(new Uri(BaseVM.downloadPath), BaseVM.fileName);
-                }
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show("error: " + ex.Message);
-            }
-            finally
-            {
-                Close();
-            }
+            DownloadService.Download(BaseVM.downloadPath, BaseVM.fileName);
+            Close();
         }
     }
 }
